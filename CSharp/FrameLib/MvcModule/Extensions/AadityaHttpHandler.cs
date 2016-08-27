88cefb9139b74cedaebdb5741f30634cad71c056
qinfengzhu <876941171@qq.com>
2016-08-27 23:35:03 +0800

    [add] MvcHttpModule  and MvcHttpHandler

---
+++ CSharp/FrameLib/MvcModule/Extensions/AadityaHttpHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcModule.Extensions
+{
+    public class AadityaHttpHandler : IHttpHandler
+    {
+        public RequestContext RequestContext { get; set; }
+        public AadityaHttpHandler(RequestContext reqcon)
+        {
+            RequestContext = reqcon;
+        }
+        public bool IsReusable
+        {
+            get 
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hello Aaditya!");
+        }
+    }
+}
---
+++ CSharp/FrameLib/MvcModule/Extensions/AadityaHttpModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MvcModule.Extensions
+{
+    public class AadityaHttpModule : IHttpModule
+    {
+        private StreamWriter sw;
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (new EventHandler(Application_BeginRequest));
+        }
+        private void Application_BeginRequest(object source, EventArgs e)
+        {
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "logger.txt";
+            if (!File.Exists(filePath))
+            {
+                sw = new StreamWriter(@"");
+            }
+            else
+            {
+                sw = File.AppendText(filePath);
+            }
+            sw.WriteLine("User sends request at {0}", DateTime.Now);
+            sw.Close();
+        }
+    }
+}
